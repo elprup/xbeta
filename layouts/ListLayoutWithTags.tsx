9279dec1b7a85f7f2cfdfa4ea1403efeccd8081c
elprup <elprup@126.com>
2023-12-28 15:19:06 +0800

    feat: show title header

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -109,6 +109,7 @@ export default function ListLayoutWithTags({
     })
 
     sortedTags = sortedTags.sort((a, b) => a.seq - b.seq)
+    title = categoryConfig[title] ? categoryConfig[title][0] : title
   }
 
   const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
@@ -119,11 +120,12 @@ export default function ListLayoutWithTags({
   return (
     <>
       <div>
-        {/* <div className="pb-6 pt-6">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+        <div className="pb-6 pt-6">
+          <h1 className="text-center text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+            {groupBy === 'tags' ? '#' : ''}
             {title}
           </h1>
-        </div> */}
+        </div>
         <div className="flex sm:space-x-24 ">
           <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
             <div className="px-6 py-4">
@@ -153,6 +155,7 @@ export default function ListLayoutWithTags({
                         </h3>
                       ) : (
                         <Link
+                          scroll={false}
                           href={`/${groupBy}/${slug(t.name)}`}
                           className={
                             t.intent == 1
